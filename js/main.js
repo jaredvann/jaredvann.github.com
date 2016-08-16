bfb1f9f04c14fc77e8fa20acf40c57590b9e33ab
Jared <jared@vann.me>
2016-08-16 16:12:38 +0100

    map update

---
+++ js/main.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFyZWR2YW5uIiwiYSI6ImF0dTVpaEUifQ.omiCGzfYx5668lX4ITRLIA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
